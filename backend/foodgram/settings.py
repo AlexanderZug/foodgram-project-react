cd7c3db49bbfd260325c7e30dff8936270019ad6
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-11-18 20:44:17 +0500

    deploy 18

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,5 +1,7 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -13,7 +15,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", default="default")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['130.193.43.83', '127.0.0.1', 'localhost']
 
 AUTH_USER_MODEL = "users.User"
 # Application definition
